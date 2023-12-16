ef426f7512afb6299d4d1d275f125e7556739e61
Jana May <jana.ricarda.may@gmail.com>
2023-12-16 09:14:32 +0100

    solve the challenge

--- js/index.js
+++ js/index.js
@@ -10,7 +10,11 @@ console.clear();
 */
 
 // --v-- write your code here --v--
-
+const title = "Social Media Post";
+const text = "Please open the console before you start.";
+const likes = 20;
+const user = "xy";
+const isReported = true;
 // --^-- write your code here --^--
 
 /*
@@ -18,5 +22,9 @@ console.clear();
 */
 
 // --v-- write your code here --v--
-
+console.log(title);
+console.log(text);
+console.log(likes);
+console.log(user);
+console.log(isReported);
 // --^-- write your code here --^--
